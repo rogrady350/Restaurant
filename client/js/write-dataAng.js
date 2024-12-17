c68fccf4de680d92a9eb810deb5d1728df815cfd
Robert OGrady <turnstoashes350@gmail.com>
2024-12-17 08:45:36 -0500

    fixed validation check on submit new record

--- client/js/write-dataAng.js
+++ client/js/write-dataAng.js
@@ -1,50 +1,63 @@
 var app = angular.module('writeDataApp', []);
 
-//POST - function to add a reservation
 app.controller("writeDataCtrl", function($scope, $http){
+    //Initialize reservation object
+    $scope.reservation = {
+        id: "",
+        name: "",
+        date: "",
+        time: "",
+        guests: "",
+        requests: "",
+        phone: "",
+        email: ""
+    }
+
+    $scope.addResults = "";
+    
+    //POST - function to submit a new reservation (write-record)
     $scope.submit = function(){
-        //make sure user does not submit blank form
-        if($scope.name === "" || $scope.date === "" || $scope.time === "" || $scope.guests === "" || $scope.phone === "" || $scope.email === "") {
-            $scope.addResults = "Please fill out all fields";
+        //Validation check. Make sure user does not submit blank form
+        if(!$scope.reservation.name || 
+            !$scope.reservation.date || 
+            !$scope.reservation.time || 
+            !$scope.reservation.guests || 
+            !$scope.reservation.phone || 
+            !$scope.reservation.email
+        ) {
+            $scope.addResults = "Please fill out all required fields";
             return;
         }
 
         $http({
             method: 'post',
             url: restaurantUrl + "/write-record",
-            data: {
-                id: $scope.id,
-                name: $scope.name,
-                date: $scope.date,
-                time: $scope.time,
-                guests: $scope.guests,
-                requests: $scope.requests,
-                phone: $scope.phone,
-                email: $scope.email
-            }
+            data: $scope.reservation,
         }).then(function(response){
             if(response.data.msg === "SUCCESS") {
                 $scope.addResults = "Reservation is added!";
-                $scope.name = "";
-                $scope.date = "";
-                $scope.time = "";
-                $scope.guests = "";
-                $scope.requests = "";
-                $scope.phone = "";
-                $scope.email = "";
+                
+                //clear form after submission
+                $scope.clear();
             }else {
                 $scope.addResults = response.data.msg
             }
         }), function(error) {
             console.log(error);
         }
-    } //end of write record function
-
-    $scope.addResults = "";
+    } //end of submit (write-record) function
 
     //clear entire form
     $scope.clear = function() {
-        document.getElementById("reservation").reset();
+        $scope.reservation = {
+            id: "",
+            name: "",
+            date: "",
+            time: "",
+            guests: "",
+            requests: "",
+            phone: "",
+            email: ""
+        }
     }
-
 });
